45a51a1c6a2c62ddfbe0e2c3ba975ee5b9bc9e35
AArtemida <19951228anna@sina.com>
2022-06-06 16:49:59 +0800

    feat: 增加翻译

--- src/lang/en.ts
+++ src/lang/en.ts
@@ -3,8 +3,61 @@
  * @Author: hy
  * @Date: 2022-05-25 09:52:42
  * @LastEditors: hy
- * @LastEditTime: 2022-05-25 10:01:22
+ * @LastEditTime: 2022-06-06 16:49:18
  */
 export default  {
-  title: 'Test'
+  backIndex: 'Back to home',
+  menu: {
+    dashboard: 'Dashboard',
+    permission: 'Permission management',
+    list: 'List',
+    manage: 'Administration',
+    theme: 'Theme',
+    errorPage: 'Error page'
+  },
+  header: {
+    userCenter: 'Personal information',
+    logout: 'Sign out',
+  },
+  dashboard: {
+    visits: 'Visits',
+    messageNumber: 'Number of messages',
+    saleVolume: 'Sales volume',
+    purchasesNumber: 'Number of purchases',
+    annualStatistics: 'Annual statistics',
+    ranking: 'Ranking',
+    topSearch: 'Top search',
+    categoryProportion: 'Category proportion'
+  },
+
+  permission: {
+    example: 'Permission example',
+    currentRole: 'Current user role',
+    switch: 'Switch permissions',
+    page: 'Page permissions',
+    button: 'Button permissions'
+  },
+
+  manage: {
+    userManage: 'User management',
+    roleManage: 'Role management',
+    permission: 'Permissions / roles',
+    description: 'Description',
+    admin: 'Administrators',
+    user: 'User',
+    editPermission: 'Edit permissions',
+    userName: 'User name',
+    operation: 'Operation',
+    edit: 'Edit',
+    delete: 'Delete',
+    searchAccount: 'Account query',
+    searchPermission: 'Permission query',
+    addUser: 'Add user',
+  },
+
+  theme: {
+    default: 'Default',
+    blue: 'Blue',
+    dark: 'Dark'
+  }
 }
--- src/lang/zh.ts
+++ src/lang/zh.ts
@@ -3,8 +3,61 @@
  * @Author: hy
  * @Date: 2022-05-25 09:52:52
  * @LastEditors: hy
- * @LastEditTime: 2022-05-25 10:01:01
+ * @LastEditTime: 2022-06-06 16:16:28
  */
 export default  {
-  title: '测试'
+  backIndex: '返回首页',
+  menu: {
+    dashboard: '工作台',
+    permission: '权限管理',
+    list: '列表',
+    manage: '管理',
+    theme: '主题',
+    errorPage: '错误页'
+  },
+  header: {
+    userCenter: '用户中心',
+    logout: '退出登录',
+  },
+  dashboard: {
+    visits: '访问量',
+    messageNumber: '消息数',
+    saleVolume: '销售额',
+    purchasesNumber: '购物数',
+    annualStatistics: '年度统计',
+    ranking: '排名',
+    topSearch: '热门搜索',
+    categoryProportion: '类别占比'
+  },
+
+  permission: {
+    example: '权限示例',
+    currentRole: '当前用户角色',
+    switch: '切换权限',
+    page: '页面权限',
+    button: '按钮权限'
+  },
+
+  manage: {
+    userManage: '用户管理',
+    roleManage: '角色管理',
+    permission: '权限/角色',
+    description: '描述',
+    admin: '管理员',
+    user: '普通用户',
+    editPermission: '编辑权限',
+    userName: '用户名',
+    operation: '操作',
+    edit: '编辑',
+    delete: '删除',
+    searchAccount: '账号查询',
+    searchPermission: '权限查询',
+    addUser: '添加用户',
+  },
+
+  theme: {
+    default: '默认',
+    blue: '蓝色',
+    dark: '深夜'
+  }
 }
